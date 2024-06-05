308967e0b0949ac00d6cf439a2c5d46d8b9bbe86
WWjj16 <lds062626@naver.com>
2024-06-05 20:46:48 +0900

    Add Unreal Engine Editor
    Add UE Editor 5.2.1 & .gitignore
    Not include StarterContent (too heavy, can't merge)

---
+++ Etheria/Source/Etheria/EtheriaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EtheriaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AEtheriaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AEtheriaGameMode();
+};
+
+
+
