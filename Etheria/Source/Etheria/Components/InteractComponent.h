7211b06e0250606c4155f0f82f0903ec999c816d
InSange <ehfrur888@naver.com>
2024-08-13 01:53:06 +0900

    컴포넌트 수정
    + 기존 ItemTestPlayer에 있던 기능들을 컴포넌트로 이전 시킴
    
    #29

--- Etheria/Source/Etheria/Components/InteractComponent.h
+++ Etheria/Source/Etheria/Components/InteractComponent.h
@@ -4,8 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Public/Interfaces/InteractionInterface.h"
 #include "InteractComponent.generated.h"
 
+class AEPlayer;
+
+USTRUCT()
+struct FInteractionData	// 상호작용에 대한 데이터
+{
+	GENERATED_USTRUCT_BODY()
+
+	FInteractionData() : CurrentInteractable(nullptr), LastInteractionCheckTime(0.0f)
+	{
+
+	};
+
+	UPROPERTY()
+	AActor* CurrentInteractable;	// 상호작용 액터
+
+	UPROPERTY()
+	float LastInteractionCheckTime;
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ETHERIA_API UInteractComponent : public UActorComponent
@@ -24,7 +43,20 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	void SetPlayer(AEPlayer* InPlayer);
+
 	void Interact();
+
+	void UpdateInteractionWidget() const;
+
+	void PerformInteractionCheck();
+	void FoundInteractable(AActor* NewInteractable);
+	void NoInteractableFound();
+	void BeginInteract();
+	void EndInteract();
+
+	// if interaction time is zero = false 
+	bool IsInteracting() const;
 	
 private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
@@ -37,7 +69,22 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float Interact_Radius = 50;
 
+protected:
+	// Interaction
+	UPROPERTY(VisibleAnywhere, Category = "Character | Interaction")
+	TScriptInterface<IInteractionInterface> TargetInteractable;	// Interaction Actor Data
+
+	float InteractionCheckFrequency;	// Interaction Frequency
+
+	float InteractionCheckDistance;	// Interaction Distance
+
+	FTimerHandle TimerHandle_Interaction;	// Check Frequency
+
+	FInteractionData InteractionData;	// Can interact Actor Info
+
 private:
 	class INPCInterface* InteractingNPC;
 	class IInteractionInterface* InteractingItem;
+	
+	AEPlayer* Player;
 };
