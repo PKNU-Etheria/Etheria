e0fa5e738b1af8f34829415fe46d98a326fa2548
WWjj16 <lds062626@naver.com>
2024-09-02 21:28:48 +0900

    Feature/Player
    Add Character AttributeSet

--- Etheria/Source/Etheria/Character/ECharacter.h
+++ Etheria/Source/Etheria/Character/ECharacter.h
@@ -4,16 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
 #include "ECharacter.generated.h"
 
 UCLASS()
-class ETHERIA_API AECharacter : public ACharacter
+class ETHERIA_API AECharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this character's properties
 	AECharacter();
+
+	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 protected:
 	// Called when the game starts or when spawned
@@ -26,4 +29,12 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+protected:
+	//ASC
+	UPROPERTY(EditAnywhere, Category = GAS)
+	TObjectPtr<class UAbilitySystemComponent> ASC;
+
+	// Attribute
+	UPROPERTY()
+	TObjectPtr<class UECharacterAttributeSet> AttributeSet;
 };
