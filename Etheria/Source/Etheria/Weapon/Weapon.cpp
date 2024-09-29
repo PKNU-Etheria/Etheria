cf16f1cd25d43824c06d4d8782f2e672e6fda69a
InSange <ehfrur888@naver.com>
2024-09-29 21:26:28 +0900

    플레이어에 부착할 웨폰 파일
    + 플레이어 소켓에 부착할 웨폰 파일
    
    #38

--- Etheria/Source/Etheria/Weapon/Weapon.cpp
+++ Etheria/Source/Etheria/Weapon/Weapon.cpp
@@ -9,6 +9,9 @@ AWeapon::AWeapon()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WEAPON"));
+
+	Weapon->SetCollisionProfileName(TEXT("WEAPON"));
 }
 
 // Called when the game starts or when spawned
